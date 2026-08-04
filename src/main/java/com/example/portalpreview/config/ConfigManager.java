47a73657a357ad3734fb88b5d04fc836ec02610f
BFTYSB <2797202622@qq.com>
2026-08-04 13:59:53 +0800

    Add src/main/java/com/example/portalpreview/config/ConfigManager.java

---
+++ src/main/java/com/example/portalpreview/config/ConfigManager.java
@@ -0,0 +1,29 @@
+package com.example.portalpreview.config;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ConfigManager {
+    private static ModConfig config;
+
+    public static void init() {
+        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+        config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+    }
+
+    public static ModConfig get() {
+        if (config == null) init();
+        return config;
+    }
+
+    public static void save() {
+        AutoConfig.getConfigHolder(ModConfig.class).save();
+    }
+
+    public static me.shedaniel.autoconfig.ConfigHolder<ModConfig> getHolder() {
+        return AutoConfig.getConfigHolder(ModConfig.class);
+    }
+}
