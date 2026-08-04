626327a4f226287fbf8b5bb6cf47dfb95b48fffe
BFTYSB <2797202622@qq.com>
2026-08-04 13:59:56 +0800

    Add src/main/java/com/example/portalpreview/integration/ModMenuIntegration.java

---
+++ src/main/java/com/example/portalpreview/integration/ModMenuIntegration.java
@@ -0,0 +1,14 @@
+package com.example.portalpreview.integration;
+
+import com.example.portalpreview.config.ModConfig;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfigClient;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfigClient.getConfigScreen(ModConfig.class, parent).get();
+    }
+}
